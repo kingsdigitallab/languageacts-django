e30ccf6166c8b885348ab80af8b5bee4ccc7db43
Ginestra Ferraro <ginestra@users.noreply.github.com>
2017-09-04 16:58:42 +0100

    Added instafeed custom

---
+++ assets/js/instafeed_lawm.js
@@ -0,0 +1,18 @@
+define(['module'], function(module) {
+    'use strict';
+
+    var feed = new Instafeed({
+        get: 'user',
+        userId: '4755811294',
+        accessToken: '4755811294.dd6559f.d035b5c4fe104bf68d57b6c6887dc2ca',
+        template: '<div class="medium-6 large-3 columns"><a href="{{link}}" target="_blank" title="Open on Instagram site in a new window"><img src="{{image}}" alt="{{caption}}"></a></div>',
+
+        // clientId: 'dd6559f3262d4ff4b533c15451b54817',
+        resolution: 'standard_resolution',
+        limit: '4'
+    });
+
+    feed.run();
+
+    return module;
+});
