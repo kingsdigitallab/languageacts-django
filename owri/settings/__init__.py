fa3e04bf3e8e4301b8217d2b015971fcb7d0102f
Ginestra Ferraro <ginestra@users.noreply.github.com>
2018-07-09 09:38:25 +0100

    Reverted __init__.py to dev

--- owri/settings/__init__.py
+++ owri/settings/__init__.py
@@ -6,4 +6,4 @@
 # imported here. Use it for settings specific to the installation and do not
 # commit to version control.
 # -----------------------------------------------------------------------------
-from liv import *  # noqa
+from dev import *  # noqa
