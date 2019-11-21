071670ddda1a372420ad0b54941117bbd63d2456
Brian Maher <brian.maher@kcl.ac.uk>
2019-11-21 14:41:09 +0000

    Updating media URL

--- owri/settings/base.py
+++ owri/settings/base.py
@@ -233,7 +233,7 @@ STATICFILES_FINDERS = (
 
 # STATICFILES_STORAGE = 'require.storage.OptimizedStaticFilesStorage' TODO
 
-MEDIA_URL = STATIC_URL + 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL.strip('/'))
 
 if not os.path.exists(MEDIA_ROOT):
