a4aaec0cb64b6d210ad703298a97fea24e509be5
elliotthall <elliott.hall@kcl.ac.uk>
2020-07-10 17:59:46 +0000

    test settings refactor

--- owri/settings/test.py
+++ owri/settings/test.py
@@ -5,6 +5,9 @@ TEMPLATE_DEBUG = True
 
 SECRET_KEY = 'test'
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
@@ -21,3 +24,17 @@ DATABASES = {
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
+
+# ActiveCollab API URL
+AC_BASE_URL = ''
+AC_API_URL = AC_BASE_URL + '/api/v1/'
+# ActiveCollab API token
+AC_TOKEN = ''
+# ActiveCollab project ID
+AC_PROJECT_ID = 0
+# ActiveCollab user ID to create the issues
+AC_USER = 0
+
+GA_ID = ''
+
+MEDIA_URL = 'media/'
